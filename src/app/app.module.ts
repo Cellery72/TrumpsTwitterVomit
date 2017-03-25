b50fd5ef025d3418ed11f2a219bf4e67d4809196
Justin <justin_ellery@hotmail.ca>
2017-03-25 12:15:10 -0400

    Added twitter-connect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { HelpModal } from '../pages/home/modal/help-modal.component';
+import { TwitterConnect } from '@ionic-native/twitter-connect';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -25,6 +26,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    TwitterConnect,
     { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { HelpModal } from './modal/help-modal.component';
 import { ModalController } from 'ionic-angular';
 import { Component } from '@angular/core';
-
+import { TwitterConnect } from '@ionic-native/twitter-connect';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -10,10 +10,23 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController, public twitter: TwitterConnect) {
 
   }
-  openModal(){
+
+  // Twitter Login Function
+  login() {
+    let onSuccess = function (response) {
+      console.log(response);
+    };
+    let onError = function (response) {
+      console.log(response);
+    };
+
+    this.twitter.login().then(onSuccess, onError);
+  }
+  // Open Informative Modal
+  openModal() {
     let helpModal = this.modalCtrl.create(HelpModal);
     helpModal.present();
   }
